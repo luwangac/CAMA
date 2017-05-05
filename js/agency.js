112de8b25856ffa4cdd2fdeb84d04109d4e75662
camaproject <admissions.cama@gmail.com>
2017-05-05 17:44:49 +0400

    "Changed many things"
    t# Please enter the commit message for your changes. Lines starting

--- js/agency.js
+++ js/agency.js
@@ -24,3 +24,12 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+})
